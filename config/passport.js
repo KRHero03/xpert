2f13770ddbcbef18c66a5e6909678230b79a9b6d
KRHero03 <krunalhero@gmail.com>
2020-11-24 14:57:29 +0530

    Initial Commit

--- config/passport.js
+++ config/passport.js
@@ -26,7 +26,7 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: env=="dev"?"http://localhost:3000/auth/google/callback":"http://localhost:"+process.env.PORT+"5000/auth/google/callback",
+      callbackURL: env=="dev"?"http://localhost:3000/auth/google/callback":"https://x-pert.herokuapp.com/auth/google/callback",
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
